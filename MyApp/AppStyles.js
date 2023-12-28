4016600218e573f91394317af403fe64d23e02fe
Robbie Christian <robbiechristian@Robbies-MacBook-Air.local>
2023-12-27 20:10:59 -0800

    starting style changes

--- MyApp/AppStyles.js
+++ MyApp/AppStyles.js
@@ -3,19 +3,22 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
     scrollView: {
-      backgroundColor: '#fff',
+      backgroundColor: '#3D437E',
     },
      
     headerText: {
       fontWeight: 'bold',
       fontSize: 18,
       padding: 10,
+      color: '#fff',
     },
     listContainer: {
-      margin: 10,
+      marginLeft: 10,
+      marginRight: 10,
       padding: 10,
       backgroundColor: '#fff',
-      borderRadius: 4,
+      
+      bordertopRadius: 0,
     },
     table: {
       borderWidth: 2,
@@ -33,10 +36,15 @@ export const styles = StyleSheet.create({
       textAlign: 'center',
     },
     inputContainer: {
-      margin: 10,
+      
+      marginLeft: 10,
+      marginRight: 10,
       padding: 10,
+      paddingTop: 30,
       backgroundColor: '#fff',
-      borderRadius: 4,
+      borderStartStartRadius: 4,
+      borderStartEndRadius: 4,
+
     },
     input: {
       height: 40,
